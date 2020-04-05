eb224b964e890e50ec8d130cfa58464f4fc5efb1
MingJian Hong <hongmingjian@gmail.com>
2020-04-05 20:06:51 +0800

    Unified line ending.

--- machdep.c
+++ machdep.c
@@ -20,7 +20,7 @@ static void init_uart(uint32_t baud)
     ra |= (2 << 12) | (2 << 15);    // alt5=TxD1/RxD1
     gpio->gpfsel1 = ra;
 
-	gpio->gppud = 0; //Disable pull-up/down
+    gpio->gppud = 0; //Disable pull-up/down
     ra = 150; while(ra--) __asm__ __volatile__("nop");
     gpio->gppudclk0 = (1 << 14) | (1 << 15);
     ra = 150; while(ra--) __asm__ __volatile__("nop");
@@ -66,70 +66,67 @@ void puts(char *s)
 __attribute__((noreturn))
 void cstart(void)
 {
-	unsigned char *where = (unsigned char *)LOADADDR;
-	init_uart(115200);
+    unsigned char *where = (unsigned char *)LOADADDR;
+    init_uart(115200);
 
-	puts("** Boot over Serial for Raspberry Pi\r\n");
-	puts("** Copyright (C) 2005,2020 MingJian Hong\r\n");
-	puts("** All rights reserved.\r\n\r\n");
+    puts("** Boot over Serial for Raspberry Pi\r\n");
+    puts("** Built at " __DATE__ " " __TIME__ "\r\n\r\n");
 
-	puts("** Built at " __DATE__ " " __TIME__ "\r\n\r\n");
+    VOLINFO volinfo;
+    unsigned char scratch[512];
+    int sdosfs = 0;
 
-	VOLINFO volinfo;
-	unsigned char scratch[512];
-	int sdosfs = 0;
+    if(0 == sdInitCard()) {
+        uint32_t pstart;
+        if((-1 != (pstart = DFS_GetPtnStart(0, scratch, 0, NULL, NULL, NULL))) &&
+           (DFS_OK == DFS_GetVolInfo(0, scratch, pstart, &volinfo)))
+            sdosfs = 1;
+    }
 
-	if(0 == sdInitCard()) {
-		uint32_t pstart;
-		if((-1 != (pstart = DFS_GetPtnStart(0, scratch, 0, NULL, NULL, NULL))) &&
-		   (DFS_OK == DFS_GetVolInfo(0, scratch, pstart, &volinfo)))
-			sdosfs = 1;
-	}
+    if(!sdosfs)
+        puts("!! WARNING: cannot write FAT file system on the SD card!\r\n");
 
-	if(!sdosfs)
-		puts("!! WARNING: cannot write FAT file system on the SD card!\r\n");
+    do {
+        puts(">> Send a uuencoded file using Xmodem protocol ...\r\n");
 
-	do {
-		puts(">> Send a uuencoded file using Xmodem protocol ...\r\n");
+        while(sys_haschar())
+            sys_getchar();
 
-		while(sys_haschar())
-			sys_getchar();
+        if(xmodem(where) > 0) {
+            char filename[16];
+            int size = uudecode(where, filename);
+            if(strncmp(filename, "kernel.img", 10) == 0) {
+                ((void (*)(void))where)();
+                break;
+            } else {
+                if(sdosfs) {
+                    FILEINFO fi;
+                    uint32_t nwritten = 0;
+                    if((DFS_OK == DFS_OpenFile(&volinfo, filename, DFS_WRITE, scratch, &fi)) &&
+                       (DFS_OK == DFS_WriteFile(&fi, scratch, where, &nwritten, size)) &&
+                       (nwritten == size)) {
+                        puts(">> Saved to ");
+                        puts(filename);
+                        puts(".\r\n");
+                    } else {
+                        puts(">> Failed to save ");
+                        puts(filename);
+                        puts(".\r\n");
+                    }
+                    DFS_Close(&fi);
+                } else {
+                    puts(">> ");
+                    puts(filename);
+                    puts(" discarded.\r\n");
+                }
+            }
+        } else {
+            puts(">> Failed to receive\r\n");
+        }
+    } while(1);
 
-		if(xmodem(where) > 0) {
-			char filename[16];
-			int size = uudecode(where, filename);
-			if(strncmp(filename, "kernel.img", 10) == 0) {
-				((void (*)(void))where)();
-				break;
-			} else {
-				if(sdosfs) {
-					FILEINFO fi;
-					uint32_t nwritten = 0;
-					if((DFS_OK == DFS_OpenFile(&volinfo, filename, DFS_WRITE, scratch, &fi)) &&
-					   (DFS_OK == DFS_WriteFile(&fi, scratch, where, &nwritten, size)) &&
-					   (nwritten == size)) {
-						puts(">> Saved to ");
-						puts(filename);
-						puts(".\r\n");
-					} else {
-						puts(">> Failed to save ");
-						puts(filename);
-						puts(".\r\n");
-					}
-					DFS_Close(&fi);
-				} else {
-					puts(">> ");
-					puts(filename);
-					puts(" discarded.\r\n");
-				}
-			}
-		} else {
-			puts(">> Failed to receive\r\n");
-		}
-	} while(1);
+    puts(">> Failed to boot!\r\n");
 
-	puts(">> Failed to boot!\r\n");
-
-	while(1)
-		;
+    while(1)
+        ;
 }
--- uudecode.c
+++ uudecode.c
@@ -11,73 +11,73 @@
 
 int uudecode (unsigned char *where, char *filename)
 {
-	char *p;
-	unsigned char *out;
-	p = out = where;
+    char *p;
+    unsigned char *out;
+    p = out = where;
 
-	{
-		int i;
-		char *q;
-		do {
-			q = p;
-			while(*p++ != '\n')
-				;
-		} while(strncmp(q, "begin ", 6));
+    {
+        int i;
+        char *q;
+        do {
+            q = p;
+            while(*p++ != '\n')
+                ;
+        } while(strncmp(q, "begin ", 6));
 
-		q += 6; //skip "begin "
+        q += 6; //skip "begin "
 
-		while(*q != ' ') q++; //skip mode
-		                 q++; //skip space
+        while(*q != ' ') q++; //skip mode
+                         q++; //skip space
 
-		i = 0;
-		while(*q != '\r' && *q != '\n') {
-			filename[i] = *q;
-			q++;
-			i++;
-			if(i == 15)
-				break;
-		}
-		filename[i] = 0;
-	}
+        i = 0;
+        while(*q != '\r' && *q != '\n') {
+            filename[i] = *q;
+            q++;
+            i++;
+            if(i == 15)
+                break;
+        }
+        filename[i] = 0;
+    }
 
-	while (1) {
-		int n;
+    while (1) {
+        int n;
 
-		while(*p == '\n')
-			p++;
+        while(*p == '\n')
+            p++;
 
-		if (!strncmp(p, "end\n", 4))
-			break;
+        if (!strncmp(p, "end\n", 4))
+            break;
 
-		if (!strncmp(p, "end\r\n", 5))
-			break;
+        if (!strncmp(p, "end\r\n", 5))
+            break;
 
-		n = DEC (*p);
+        n = DEC (*p);
 
-		if (n <= 0)
-			break;
+        if (n <= 0)
+            break;
 
-		for (++p; n > 0; p += 4, n -= 3) {
-			if (n >= 3)	{
-				*out++ = DEC (p[0]) << 2 | DEC (p[1]) >> 4;
-				*out++ = DEC (p[1]) << 4 | DEC (p[2]) >> 2;
-				*out++ = DEC (p[2]) << 6 | DEC (p[3]);
-			} else {
-				if (n >= 1)	{
-					*out++ = DEC (p[0]) << 2 | DEC (p[1]) >> 4;
-				}
-				if (n >= 2)	{
-					*out++ = DEC (p[1]) << 4 | DEC (p[2]) >> 2;
-				}
-			}
-		}
+        for (++p; n > 0; p += 4, n -= 3) {
+            if (n >= 3) {
+                *out++ = DEC (p[0]) << 2 | DEC (p[1]) >> 4;
+                *out++ = DEC (p[1]) << 4 | DEC (p[2]) >> 2;
+                *out++ = DEC (p[2]) << 6 | DEC (p[3]);
+            } else {
+                if (n >= 1) {
+                    *out++ = DEC (p[0]) << 2 | DEC (p[1]) >> 4;
+                }
+                if (n >= 2) {
+                    *out++ = DEC (p[1]) << 4 | DEC (p[2]) >> 2;
+                }
+            }
+        }
 
-		/*discard rest of the line*/
-		while(*p != '\n')
-			p++;
-	}
+        /*discard rest of the line*/
+        while(*p != '\n')
+            p++;
+    }
 
-	return (out-where);
+    return (out-where);
 }
 
 #if __TESTING__
@@ -87,53 +87,52 @@ int uudecode (unsigned char *where, char *filename)
 
 int main()
 {
-	FILE *fp;
-	long size;
-	char *buf;
-	char filename[16];
+    FILE *fp;
+    long size;
+    char *buf;
+    char filename[16];
 
+    fprintf(stdout, "\\n=%x, \\r=%x\n", '\n', '\r');
 
-	fprintf(stdout, "\\n=%x, \\r=%x\n", '\n', '\r');
+    fp = fopen("kernel.uue", "rb");
+    if(fp == NULL) {
+        fprintf(stderr, "error fopen");
+        return 1;
+    }
 
-	fp = fopen("kernel.uue", "rb");
-	if(fp == NULL) {
-		fprintf(stderr, "error fopen");
-		return 1;
-	}
+    fseek(fp, 0, SEEK_END);
+    size = ftell(fp);
+    rewind(fp);
 
-	fseek(fp, 0, SEEK_END);
-	size = ftell(fp);
-	rewind(fp);
+    fprintf(stdout, "size=%d\n", size);
 
-	fprintf(stdout, "size=%d\n", size);
+    buf = (char *)malloc(size);
+    {
+        int read = fread(buf, 1, size, fp);
+        if( read != size) {
+            fprintf(stderr, "error fread (%d expected, %d read)\n", size, read);
+            free(buf);
+            fclose(fp);
+            return 1;
+        }
+    }
+    fclose(fp);
 
-	buf = (char *)malloc(size);
-	{
-		int read = fread(buf, 1, size, fp);
-		if( read != size) {
-			fprintf(stderr, "error fread (%d expected, %d read)\n", size, read);
-			free(buf);
-			fclose(fp);
-			return 1;
-		}
-	}
-	fclose(fp);
+    fp = fopen("kernel.uud", "wb");
+    if(fp == NULL) {
+        fprintf(stderr, "error fopen");
+        free(buf);
+        return 1;
+    }
 
-	fp = fopen("kernel.uud", "wb");
-	if(fp == NULL) {
-		fprintf(stderr, "error fopen");
-		free(buf);
-		return 1;
-	}
+    size = uudecode(buf, filename);
+    fwrite(buf, 1, size, fp);
 
-	size = uudecode(buf, filename);
-	fwrite(buf, 1, size, fp);
+    fprintf(stdout, "%d bytes decoded.\n", size);
 
-	fprintf(stdout, "%d bytes decoded.\n", size);
-
-	free(buf);
-	fclose(fp);
-	return 0;
+    free(buf);
+    fclose(fp);
+    return 0;
 }
 
 #endif
